1777fca2a563b62d634b01a094c8c0c79e4cb1b7
kothainagaraj5154 <kothainagarajcg2@gmail.com>
2024-09-09 19:43:45 +0530

    Basics of MAUI

--- Maui_Navigation_EventHandling_Task1/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/Maui_Navigation_EventHandling_Task1.AssemblyInfo.cs
+++ Maui_Navigation_EventHandling_Task1/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/Maui_Navigation_EventHandling_Task1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Maui_Navigation_EventHandling_Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4844944762e77e10992f65f4ab2f65ebcebae496")]
 [assembly: System.Reflection.AssemblyProductAttribute("Maui_Navigation_EventHandling_Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Maui_Navigation_EventHandling_Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
